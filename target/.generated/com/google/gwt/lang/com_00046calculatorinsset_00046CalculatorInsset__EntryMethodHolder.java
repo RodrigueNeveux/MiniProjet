b375611ac6aa7b7906896872ea10d473e258a2a6
Rodrigue <rodrigueneveux19@gmail.com>
2025-10-24 10:28:40 +0200

    Fix : Finalisation

--- target/.generated/com/google/gwt/lang/com_00046calculatorinsset_00046CalculatorInsset__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class com_00046calculatorinsset_00046CalculatorInsset__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
